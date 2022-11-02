df501fbb327d404382ded1dd05ac847c711ccc94
raj purkait <rajpurkait9@gmail.com>
2022-11-02 18:06:42 +0530

    update code for check password in login api

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -37,13 +37,17 @@ const login = async (req, res) => {
       return res
         .status(404)
         .send(`user is not register with email id ${email}`);
+    const isPasswordSame = await bcrypt.compare(password, IsUser.password);
+    if (!isPasswordSame) return res.send("password is wrong..");
     // json web token genrate code
     const token = await jwt.sign(
       { userId: IsUser._id, email: IsUser.email, userName: IsUser.name },
       process.env.JWT_SECRET,
       { expiresIn: "30d" }
     );
-    IsUser.updateOne({token},{new:true,runValidation : true}).then().catch(err=>console.log(err));
+    IsUser.updateOne({ token }, { new: true, runValidation: true })
+      .then()
+      .catch((err) => console.log(err));
     res.send({ msg: `login sucessfull..`, token });
   } catch (error) {
     res.status(500).send({ msg: error.message });
